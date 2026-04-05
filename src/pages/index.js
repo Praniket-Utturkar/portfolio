a6a7e80955f544f558466663ba2bda005980609a
Praniket Utturkar <praniketutturkane4444@gmail.com>
2026-04-05 15:09:34 +0530

    Renamed title and desc

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,12 +30,12 @@ return (
          />
         </div>
         <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
-          <AnimatedText text="Turning Vision Into Reality With Code And Design." className='!text-6xl !text-left 
+          <AnimatedText text="Turning Vision Into Reality With AI" className='!text-6xl !text-left 
           xl:!text-5xl lg:!text-center lg:!text=6xl md:!text-5xl sm:!text-3xl 
           '/>
           <p className='my-4 text-base font-medium md:text-sm sm:text-xs '>
-            As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. 
-            Explore my latest projects and articles, showcasing my expertise in MERN Stack and web development.
+            As a skilled AI Engineer, I am dedicated to turning ideas into innovative AI web applications. 
+            Explore my latest projects and articles, showcasing my expertise in MERN Stack and AI.
             
           </p>
           <div className='flex items-center self-start mt-2 lg:self-center'>
